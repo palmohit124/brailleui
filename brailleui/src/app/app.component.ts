0b6a7f5db13d222a9ba2c3e0138b210f184e322d
Mohit <mohit.pal@ignitesol.com>
2019-09-29 19:06:00 +0530

    First page grade 1

--- brailleui/src/app/app.component.ts
+++ brailleui/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  converterForm = new FormGroup({
+    grade: new FormControl('', Validators.required),
+    standard: new FormControl('', Validators.required),
+    input: new FormControl('', Validators.required)
+  });
+  output: any;
+
+  grades = [
+    "Grade 1",
+    "Grade 2",
+    "Grade 3"
+  ];
+
+  standards = [
+    "Standard"
+  ]
+
+  onSubmit() {
+    if ( this.converterForm.controls['grade'].value === 'Grade 1' ) {
+      this.output = this.converterForm.controls['input'].value; 
+    }
+  }
+
+  onClear() {
+    this.converterForm.controls['input'].setValue('');
+    this.output = "";
+  }
+
 }
--- brailleui/src/app/app.module.ts
+++ brailleui/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
